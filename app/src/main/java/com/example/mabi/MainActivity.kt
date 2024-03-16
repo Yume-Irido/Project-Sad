32b5c54abd79ee29695ed2e44c71377d7301c365
Yume-Irido <galangsean1@gmail.com>
2024-03-17 07:52:38 +0800

    initial commit

---
+++ app/src/main/java/com/example/mabi/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mabi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+//        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
